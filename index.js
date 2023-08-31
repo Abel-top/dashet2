34081d18670c0e24ae417641bb8333104b893da7
Abel <jidaabel1111@gmail.com>
2023-08-31 13:41:51 -0700

    initial

---
+++ index.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll',function(){
+    var element=document.querySelector('#wel');
+    var position=element.getBoundingClientRect().top;
+    var winHeight=this.window.innerHeight;
+    if(position>windowHeight){
+        element.classList.add('animate');
+    }
+
+});
