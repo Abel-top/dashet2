739b6b484b9224041e308e2137d11d2c8b89b494
Abel <jidaabel1111@gmail.com>
2023-09-02 19:08:10 -0700

    intialcommit

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-const the_animation = document.querySelectorAll('.wel');
 const ela =document.getElementById("mukera");
 ela.innerHTML="mukera new";
 
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('wel-anim')
-        }
-            else {
-                entry.target.classList.remove('wel-anim')
-            }
-        
-    })
-},
-   { threshold: 0.5
-   });
-//
-  for (let i = 0; i < the_animation.length; i++) {
-   const elements = the_animation[i];
+// const the_animation = document.querySelectorAll('.wel');
 
-    observer.observe(elements);
-  } 
+
+
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add('wel-anim')
+//         }
+//             else {
+//                 entry.target.classList.remove('wel-anim')
+//             }
+        
+//     })
+// },
+//    { threshold: 0.5
+//    });
+// //
+//   for (let i = 0; i < the_animation.length; i++) {
+
+//     const elements = the_animation[i];
+
+//     observer.observe(elements);
+//   } 
